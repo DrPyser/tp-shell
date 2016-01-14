87f2615633465d86da71436487dddce984284d94
drpyser <schok53@gmail.com>
2016-01-13 23:13:28 -0500

    J'ai prototypé un read-print loop functionel qui utilise un buffer pour lire une ligne et l'imprimer.

--- ch.c
+++ ch.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
  *Writes the data from the stream "from" to a file named "filename".
@@ -53,23 +53,30 @@ int pipe(FILE* from, FILE* to){
   while((c = fgetc(from)) != EOF){
     ret_code = fputc(c,to);
     if(ret_code == EOF){
-      fprintf(stderr,"failure to write character %c in file %s", c, filename);
-      fclose(output);
+      fprintf(stderr,"failure to write character %c to output stream", c);
       return EXIT_FAILURE;
     }
   }
   fflush(from);
   return EXIT_SUCCESS;
-    
 }
 
 //int inputFrom(FILE* to, FILE* input
 
-int main (void)
+int main (int argc, char* argv[])
 {
   fprintf (stdout, "%% ");
 
   /* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter. */
+  //int c;
+  //int ret_code;
+  char buffer[255];
+  FILE* output = stdout;
+  FILE* input = (argc > 1)?fopen(argv[1], "r"):stdin;
+  while(fgets(buffer, 255, input) != NULL && strcmp(buffer,"quit\n") != 0){
+    fprintf(output, buffer);
+    fprintf (stdout, "%% ");
+  }
 
   fprintf (stdout, "Bye!\n");
   exit (0);
